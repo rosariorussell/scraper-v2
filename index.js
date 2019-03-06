3727f58d87913740b05bb5f163633b1b60cfdba8
Russell Rosario <rosariorussell@gmail.com>
2019-03-06 17:08:21 -0500

    updated to save to pipe separated csv

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const puppeteer = require('puppeteer')
+const fs = require('fs-extra')
 
 const cssSelector = 'input#viewprofile'
-
 const website = 'https://aiamiami.org/membership/members-directory/'
+
+const results = []
 
 const scrape = async (pageToScrape) => {
   // SET UP PUPPETEER
@@ -13,7 +15,6 @@ const scrape = async (pageToScrape) => {
   // START SCRAPING
   await page.goto(pageToScrape, { 'waitUntil': 'networkidle0' })
 
-  const results = []
 
   await page.$$eval(cssSelector, el => el[0].click())
   await page.waitForSelector('h2')
@@ -35,7 +36,6 @@ const scrape = async (pageToScrape) => {
   await page.waitForSelector('h2')
   name = await page.evaluate(()=> document.querySelector('td strong').innerText)
   company = await page.evaluate(()=> Array.from(document.querySelectorAll('tr td'))[1].innerText)
-
   phone = await page.evaluate(()=> Array.from(document.querySelectorAll('tr td'))[7].innerText)
   email = await page.evaluate(()=> Array.from(document.querySelectorAll('tr td'))[13].innerText)
   website = await page.evaluate(()=> Array.from(document.querySelectorAll('tr td'))[16].innerText)
@@ -52,7 +52,6 @@ const scrape = async (pageToScrape) => {
   await page.waitForSelector('h2')
   name = await page.evaluate(()=> document.querySelector('td strong').innerText)
   company = await page.evaluate(()=> Array.from(document.querySelectorAll('tr td'))[1].innerText)
-
   phone = await page.evaluate(()=> Array.from(document.querySelectorAll('tr td'))[7].innerText)
   email = await page.evaluate(()=> Array.from(document.querySelectorAll('tr td'))[13].innerText)
   website = await page.evaluate(()=> Array.from(document.querySelectorAll('tr td'))[16].innerText)
@@ -65,15 +64,13 @@ const scrape = async (pageToScrape) => {
   await page.goBack()
 
 
-
-  const fs = require('fs-extra')
-  for (let result of results) {
-    await fs.appendFile('results.csv', `${result.name},${result.company},${result.phone},${result.email},${result.website}\n`)
-  }
-
-
+  // CLOSE BROWSER
   await browser.close()
 
+  // SAVE DATA TO PIPE SEPARATED CSV
+  for (let result of results) {
+    await fs.appendFile('results.csv', `${result.name}|${result.company}|${result.phone}|${result.email}|${result.website}\n`)
+  }
 }
 
 scrape(website)
